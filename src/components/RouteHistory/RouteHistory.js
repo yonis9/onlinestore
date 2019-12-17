8a93944a3be7a3ba152929a7b2b206f71d44f66d
Yoni Sisso <yonis9@walla.com>
2019-12-17 14:30:49 +0200

    updates

--- src/components/RouteHistory/RouteHistory.js
+++ src/components/RouteHistory/RouteHistory.js
@@ -2,7 +2,7 @@ import { withRouter } from 'react-router-dom';
 import React from 'react'
 
 const RouteHistory = ({ history, onMobileRouteChange }) => {
-
+console.log(history)
     history.listen((location, action) => {
         // location is an object like window.location
         onMobileRouteChange()
