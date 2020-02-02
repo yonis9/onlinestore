5a8813097b0ae0ac8fb312d9ee31f85bde15dbf9
Yoni Sisso <yonis9@walla.com>
2020-02-02 20:34:02 +0200

    fixing loading view

--- src/components/Section3/Section3.js
+++ src/components/Section3/Section3.js
@@ -8,17 +8,26 @@ const Section3 = ({ randProducts, onAddToCart }) => {
     console.log(randProducts)
     return (
         <div className='sec3'>
-            <div className='home-products'>
-            {randProducts.length ? randProducts.map(product => {
-                return <ProductCard key={product._id}
+            {
+                randProducts.length ? 
+                <div className='home-products'>
+               { randProducts.map(product => 
+                 <ProductCard key={product._id}
                                     name={product.name}
                                     imagePath={product.imagePath} 
                                     price={product.price}
                                     description={product.description} 
                                     id={product._id} 
                                     onAddToCart={onAddToCart}/>
-            }): <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>}
+            )}
             </div>
+            :
+           
+                <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+            
+            }
+
+            
             <button id='sec3btn'><Link to="/shop">SEE ALL PRODUCTS</Link></button>
         </div>
     )
